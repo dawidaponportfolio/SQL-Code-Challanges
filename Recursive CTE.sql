d03977c29a958fbd93e0e249e07eea8c5fd82af2
Dawid <da.dawid.apon@gmail.com>
2025-08-29 12:45:05 +0200

    Add files via upload

---
+++ Recursive CTE.sql
@@ -0,0 +1,14 @@
+WITH RECURSIVE company_hierarchy AS 
+(
+SELECT Employee_ID, 1 AS Level 
+FROM  hierarchy
+WHERE supervisor_id IS NULL
+UNION ALL
+SELECT eh.Employee_ID, Level + 1
+FROM hierarchy eh
+JOIN company_hierarchy ch
+ON eh.supervisor_id = ch.employee_id
+)
+SELECT Employee_ID, Level
+FROM company_hierarchy
+ORDER BY Employee_ID ASC;
